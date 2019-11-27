83c8565be820e04e967031658b95f66057876190
Thomas Marquis <thomas.marquis@ext.cdiscount.com>
2019-11-27 11:40:35 +0100

    leader board secondary content

--- LeaderBoardScreen/index.js
+++ LeaderBoardScreen/index.js
@@ -1,22 +1,29 @@
+import capitalize from "capitalize";
 import React from "react";
-import { Button } from "react-native-elements";
+import { Platform } from "react-native";
+import { Button, Text } from "react-native-elements";
 
-import { CDISCOUNT } from "../routes";
 import Screen from "../Screen";
+import { CDISCOUNT } from "../routes";
+import { SecondaryContent } from "./styles";
 
 export default function LeaderBoardScreen({ navigation }) {
   const goToCdiscountScreen = () => {
     navigation.navigate(CDISCOUNT);
   };
+  const deviceVersion = capitalize(`${Platform.OS} v.${Platform.Version}`);
 
   return (
     <Screen title="Leader Board">
-      <Button
-        title={CDISCOUNT}
-        type="solid"
-        raised
-        onPress={goToCdiscountScreen}
-      />
+      <SecondaryContent>
+        <Text>{deviceVersion}</Text>
+        <Button
+          title={CDISCOUNT}
+          type="solid"
+          raised
+          onPress={goToCdiscountScreen}
+        />
+      </SecondaryContent>
     </Screen>
   );
 }
---
+++ LeaderBoardScreen/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const SecondaryContent = styled.View({
+  width: "80%",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-around"
+});
